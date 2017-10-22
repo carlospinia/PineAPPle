089054402d56f3d4d2d7446454eaf998ca3d7b14
carlospinia <carlos.p.m.2293@gmail.com>
2017-10-23 00:23:54 +0200

    añadido menu lateral con funcionamiento parcial, falta terminarlo.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,37 +1,117 @@
 package com.example.myfirstapp;
 
-import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar;
+        toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_name);
+
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        NavigationView navView;
+        navView = (NavigationView) findViewById(R.id.navview);
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem){
+                        boolean fragmentTransaction = false;
+                        Fragment fragment = null;
+
+                        switch (menuItem.getItemId()){
+                            case R.id.menu_seccion_1:
+                                fragment = new FragmentThree();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.menu_seccion_2:
+                                fragment = new FragmentOne();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.menu_seccion_3:
+                                fragment = new FragmentTwo();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.menu_opcion_1:
+                                Log.i("NavigationView", "Pulsada opcion 1");
+                                break;
+                            case R.id.menu_opcion_2:
+                                Log.i("NavigationView", "Pulsada opcion 2");
+                                break;
+                        }
+
+                        if (fragmentTransaction){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.pager, fragment).commit();
+                            menuItem.setChecked(true);
+                            getSupportActionBar().setTitle(menuItem.getTitle());
+                        }
+
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                }
+        );
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         //Add fragments to adapter one by one
-        adapter.addFragment(new FragmentOne(), "Rosa");
-        adapter.addFragment(new FragmentTwo(), "Azul");
-        adapter.addFragment(new FragmentThree(), "Peliculas");
-        viewPager.setAdapter(adapter);
+//        adapter.addFragment(new FragmentOne(), "Rosa");
+//        adapter.addFragment(new FragmentTwo(), "Azul");
+//      adapter.addFragment(new FragmentThree(), "Peliculas");
+//        viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        tabLayout.setupWithViewPager(viewPager);
+
+        //Floating Action Button
+        FloatingActionButton btnFab = (FloatingActionButton)findViewById(R.id.btnFab);
+        btnFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "¡NO TOQUES! ¿Por qué tocas?", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     // Adapter for the viewpager using FragmentPagerAdapter
@@ -63,4 +143,6 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+
 }
