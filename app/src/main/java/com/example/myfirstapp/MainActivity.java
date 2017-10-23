5919f22b575d6423cb840fb0bb4b595150a6294e
Carlos Piña Martínez <carlos.p.m.2293@gmail.com>
2017-10-23 11:17:36 +0200

    Menu lateral terminado. Añadida imagen a la izquierda en las tarjetas y snackbars al pulsar en las opciones 1 y 2 del menu lateral (fijadas al floating button para que no se superpongan).

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -35,8 +36,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_name);
 
-
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        Fragment fragment = new FragmentThree();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame ,fragment).commit();
+
+        //Floating Action Button
+        final FloatingActionButton btnFab = (FloatingActionButton)findViewById(R.id.btnFab);
+        btnFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "¡NO TOQUES! ¿Por qué tocas?", Snackbar.LENGTH_LONG).show();
+            }
+        });
 
         NavigationView navView;
         navView = (NavigationView) findViewById(R.id.navview);
@@ -49,27 +61,27 @@ public class MainActivity extends AppCompatActivity {
 
                         switch (menuItem.getItemId()){
                             case R.id.menu_seccion_1:
-                                fragment = new FragmentThree();
-                                fragmentTransaction = true;
-                                break;
-                            case R.id.menu_seccion_2:
                                 fragment = new FragmentOne();
                                 fragmentTransaction = true;
                                 break;
-                            case R.id.menu_seccion_3:
+                            case R.id.menu_seccion_2:
                                 fragment = new FragmentTwo();
                                 fragmentTransaction = true;
                                 break;
+                            case R.id.menu_seccion_3:
+                                fragment = new FragmentThree();
+                                fragmentTransaction = true;
+                                break;
                             case R.id.menu_opcion_1:
-                                Log.i("NavigationView", "Pulsada opcion 1");
+                                Snackbar.make(btnFab, "Pulsada opción 1", Snackbar.LENGTH_LONG).show();
                                 break;
                             case R.id.menu_opcion_2:
-                                Log.i("NavigationView", "Pulsada opcion 2");
+                                Snackbar.make(btnFab, "Pulsada opción 2", Snackbar.LENGTH_LONG).show();
                                 break;
                         }
 
                         if (fragmentTransaction){
-                            getSupportFragmentManager().beginTransaction().replace(R.id.pager, fragment).commit();
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
                             menuItem.setChecked(true);
                             getSupportActionBar().setTitle(menuItem.getTitle());
                         }
@@ -80,69 +92,17 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        //Add fragments to adapter one by one
-//        adapter.addFragment(new FragmentOne(), "Rosa");
-//        adapter.addFragment(new FragmentTwo(), "Azul");
-//      adapter.addFragment(new FragmentThree(), "Peliculas");
-//        viewPager.setAdapter(adapter);
-
-//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-//        tabLayout.setupWithViewPager(viewPager);
-
-        //Floating Action Button
-        FloatingActionButton btnFab = (FloatingActionButton)findViewById(R.id.btnFab);
-        btnFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "¡NO TOQUES! ¿Por qué tocas?", Snackbar.LENGTH_LONG).show();
-            }
-        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch(item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    // Adapter for the viewpager using FragmentPagerAdapter
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
-
 
 }
