730e1277942de8c7971578610428bf6673274c45
Carlos Piña Martínez <carlos.p.m.2293@gmail.com>
2017-10-25 14:59:58 +0200

    Seccion 2 ahora es Mapa y contiene un mapa de google.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,22 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String ITEM_NAME = "com.example.myfirstapp.ITEM";
@@ -67,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
                                 fragmentTransaction = true;
                                 break;
                             case R.id.menu_seccion_2:
-                                //fragment = new FragmentTwo();
-                                //fragmentTransaction = true;
                                 String itemMenuName = (String) menuItem.getTitle();
                                 openSec1(getWindow().getDecorView().getRootView(), itemMenuName);
                                 break;
--- app/src/main/java/com/example/myfirstapp/Seccion1.java
+++ app/src/main/java/com/example/myfirstapp/Seccion1.java
@@ -1,12 +1,23 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class Seccion1 extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Seccion1 extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,29 @@ public class Seccion1 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Fragment fragment;
-        fragment = new FragmentThree();
-        getSupportFragmentManager().beginTransaction().replace(R.id.layout_secc1, fragment).commit();
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button buttonMarcador = (Button) findViewById(R.id.addMarcador);
+        buttonMarcador.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addMarcador();
+            }
+        });
 
         Intent intent = getIntent();
         String item_name = intent.getStringExtra(MainActivity.ITEM_NAME);
         getSupportActionBar().setTitle(item_name);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+    }
+
+    private void addMarcador(){
+        mapa.addMarker(new MarkerOptions()
+                .position(new LatLng(40.3936945, -3.701519)));
     }
 }
